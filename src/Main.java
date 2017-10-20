fea1ea4f3de968f527562f78e0cff6bc4eb8e9de
wjbigari <wjbigari@gmail.com>
2017-10-19 19:59:01 -0500

    json

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import javax.json.JsonObject;
 import java.net.Socket;
 
+
 public class Main {
     public static void Main(String [] args){
+        public static final int PORTNUMBER = 8081;
 
     }
 
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,12 @@
-public class Server {
+import java.net.ServerSocket;
+
+public class Server extends Thread{
+
+    private ServerSocket socket;
+    public Serversocket(ServerSocket socket){
+        this.socket = socket;
+
+    }
+
+
 }
